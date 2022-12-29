14fc4f2eb0da7492ad70111e00c95835cb82523c
mariamayman29 <121639712+mariamayman29@users.noreply.github.com>
2022-12-29 23:47:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Enter two integers to know the perfect numbers between them");
+
+int num1 = int.Parse(Console.ReadLine());
+
+int num2 = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+if (num1 <= 0 )
+{
+    Console.WriteLine("there is no perfect numbers");
+}
+
+else
+{
+
+    for (; num1 <= num2; num1++)
+    {
+
+        for (int i = 1; i < num1 ; i++)
+        {
+
+            if (num1 % i == 0)
+            {
+                sum = sum + i;
+            }
+        }
+        if (sum == num1)
+
+        {
+            Console.WriteLine("{0} is a perfect number", num1);
+        }
+        sum = 0; 
+        
+    }
+}
+
+
